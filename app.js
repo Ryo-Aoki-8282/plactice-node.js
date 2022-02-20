a48d3026543efd78fca6298bcc8249a7d872ae3b
Ryo-Aoki-8282 <too8282_late@icloud.com>
2022-02-20 16:39:16 +0900

    setting ejs

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const PORT = process.env.PORT;
-console.log(`Application listeining at ${PORT}`);
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, () => {
+  console.log(`Application listening at :${PORT}`);
+});
