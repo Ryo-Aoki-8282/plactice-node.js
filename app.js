ba09daf70e2d2d7d5d3f125582343caeeb3e0cd1
Ryo-Aoki-8282 <too8282_late@icloud.com>
2022-02-24 21:32:03 +0900

    create detaile page

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
+// Expose global method to
+app.use((req, res, next)=> {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 // Static resource routing
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -18,18 +25,8 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 // Set access log
 app.use(accessLogger());
 // Dynamic resource routing
+app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
-app.use("/test", async (req, res, next) => {
-  const { MySQLClient, sql } = require("./lib/database/client.js");
-  var data;
-  try {
-    data = await MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"), [1]);
-    console.log(data);
-  } catch(err) {
-    next(err);
-  }
-  res.end("ok");
-});
 
 // Set application log
 app.use(applicationlogger());
