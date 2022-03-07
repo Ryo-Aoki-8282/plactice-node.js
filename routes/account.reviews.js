e54d98d6fd4811d7b47b6a764a3a7b9425fbb5d5
Ryo-Aoki-8282 <too8282_late@icloud.com>
2022-03-07 19:48:31 +0900

    create review page complete

--- routes/account.reviews.js
+++ routes/account.reviews.js
@@ -7,12 +7,12 @@ var validateReviewData = function (req) {
   var body = req.body;
   var isValid = true, error = {};
 
-  if(body.visit && !moment(body.visit, DATE_FORMAT).isValid()){
+  if (body.visit && !moment(body.visit, DATE_FORMAT).isValid()) {
     isValid = false;
     error.visit = "訪問日の日付文字列が不正です。";
   }
 
-  if(isValid) {
+  if (isValid) {
     return undefined;
   }
   return error;
@@ -33,7 +33,7 @@ var createReviewData = function (req) {
 router.get("/regist/:shopId(\\d+)", async (req, res, next) => {
   var shopId = req.params.shopId;
   var shop, shopName, review, results;
-  
+
   try {
     results = await MySQLClient.executeQuery(
       await sql("SELECT_SHOP_BASIC_BY_ID"),
@@ -42,24 +42,24 @@ router.get("/regist/:shopId(\\d+)", async (req, res, next) => {
     shop = results[0] || {};
     shopName = shop.name;
     review = {};
-    res.render("./account/reviews/regist-form.ejs", {shopId, shopName, review});
-  } catch(err) {
+    res.render("./account/reviews/regist-form.ejs", { shopId, shopName, review });
+  } catch (err) {
     next(err);
   }
 });
 
-router.post("/regist/:shopId(\\d+)", (req, res, next) => {
+router.post("/regist/:shopId(\\d+)", async (req, res, next) => {
   var review = createReviewData(req);
-  var {shopId, shopName} = req.body;
+  var { shopId, shopName } = req.body;
   res.render("./account/reviews/regist-form.ejs", { shopId, shopName, review });
 });
 
-router.post("/regist/confirm", (req, res, next) => {
+router.post("/regist/confirm", (req, res) => {
   var error = validateReviewData(req);
   var review = createReviewData(req);
-  var {shopId, shopName} = req.body;
+  var { shopId, shopName } = req.body;
 
-  if(error){
+  if (error) {
     res.render("./account/reviews/regist-form.ejs", { error, shopId, shopName, review });
     return;
   }
@@ -67,5 +67,39 @@ router.post("/regist/confirm", (req, res, next) => {
   res.render("./account/reviews/regist-confirm.ejs", { shopId, shopName, review });
 });
 
+router.post("/regist/execute", async (req, res, next) => {
+  var error = validateReviewData(req);
+  var review = createReviewData(req);
+  var { shopId, shopName } = req.body;
+  var userId = "1";     // TODO: ログイン実装後に更新
+  var transaction;
+
+  if (error) {
+    res.render("./account/reviews/regist-form.ejs", { error, shopId, shopName, review });
+    return;
+  }
+
+  try {
+    transaction = await MySQLClient.beginTransaction();
+    transaction.executeQuery(
+      await sql("SELECT_SHOP_BY_ID_FOR_UPDATE"),
+      [shopId]
+    );
+    transaction.executeQuery(
+      await sql("INSERT_SHOP_REVIEW"),
+      [shopId, userId, review.score, review.visit, review.description]
+    );
+    transaction.executeQuery(
+      await sql("UPDATE_SHOP_SCORE_BY_ID"),
+      [shopId, shopId]
+    );
+    await transaction.commit();
+  } catch (err) {
+    await transaction.rollback();
+    next(err);
+  }
+
+  res.render("./account/reviews/regist-complete.ejs");
+});
 
 module.exports = router;
